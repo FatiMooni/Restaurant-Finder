3664c9a6de217c503914d69df9136490252f9cd0
Fatima <abdiche.fz@datafirst-dz.com>
2020-11-12 14:31:26 +0000

    first commit

---
+++ android/app/src/main/java/com/raywenderlich/restaurant_finder/MainActivity.java
@@ -0,0 +1,12 @@
+package com.raywenderlich.restaurant_finder;
+import io.flutter.embedding.android.FlutterActivity;
+
+
+
+public class MainActivity extends FlutterActivity {
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    GeneratedPluginRegistrant.registerWith(this);
+//  }
+}
